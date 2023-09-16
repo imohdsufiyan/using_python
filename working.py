73d60daa073a1b36d02a5a4e7a1e454d5f14fd03
imohdsufiyan <94213660+imohdsufiyan@users.noreply.github.com>
2023-09-16 18:24:55 +0000

    updated

--- working.py
+++ working.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 
 # Sample DataFrame
@@ -7,7 +8,7 @@ data = {
     'Age': [25, 30, 35, 40],
     'City': ['New York', 'Los Angeles', 'Chicago', 'Houston'],
     'Visit': [5, 10, 15, 20],
-    'Profit': [100, 200, 300, 400]
+    'Profit': [-100, 200, 300, 400]
 }
 
 df = pd.DataFrame(data)
@@ -15,26 +16,34 @@ df = pd.DataFrame(data)
 
 
 
+# nested if 
 
-# Sample DataFrame
-data = {
-    'Name': ['Alice', 'Bob', 'Charlie', 'David'],
-    'Age': [25, 30, 35, 40],
-    'City': ['New York', 'Los Angeles', 'Chicago', 'Houston'],
-    'Visit': [5, 10, 15, 20],
-    'Profit': [100, 200, 300, 400]
-}
+## np.where
+df['remarks_1'] = np.where(df['Profit']<= 110,"V Low",
+                         np.where(df['Profit']<= 200,"Low",
+                                  np.where(df['Profit']<= 300,"Medium",
+                                           "High")))
 
-df = pd.DataFrame(data)
+df['remarks_X'] = np.where((df['Profit']<= 200) | (df['City']== "New York"), "A","B")
 
-# Selecting columns that contain only non-numeric data
-non_numeric_columns = df.select_dtypes(include=['number'],)
+## pd.cut
+bins = [df['Profit'].min(), 110,200,300,400]
+labels = ['V Low', 'Low','Medium','High']
+df['remarks_2'] = pd.cut(df['Profit'],bins = bins, labels = labels, include_lowest= True)
 
-# Displaying the selected columns
-print(non_numeric_columns)
 
-# Selecting columns that contain "it" in their names
-selected_columns = df.filter(like='it', axis=1)
+## custom function
+def remarks_func(x):
+    if x <= 110 :
+        y = "V Low"
+    elif x <= 200 :
+        y = "Low"
+    elif x<= 300 :
+        y = "Medium"
+    else :
+        y = "High"
+    return y
+df['remarks_3'] = df['Profit'].apply(remarks_func)
 
-# Displaying the selected columns
-print(selected_columns)
+print(df)
+
