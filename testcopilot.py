5f10656e370de90c20422bc136c90ff4d82823a3
imohdsufiyan <94213660+imohdsufiyan@users.noreply.github.com>
2023-10-02 11:01:17 +0000

    u

--- testcopilot.py
+++ testcopilot.py
@@ -1,24 +1,4 @@
-import pandas as pd
-
-
-
-
-# show use of list.index() method
-# create a list
-list1 = ['physics', 'chemistry', 'mathematics']
-# call index() method
-# returns index of 'chemistry'
-print('Index of chemistry:', list1.index('chemistry'))
-
-
-list1.insert(1, 'geography')
-print('Updated List:', list1)
-
-list1.extend(['history', 'civics'])
-print('Updated List:', list1)
-
-list1.pop(1)
-print('Updated List:', list1)
+git config pull.rebase false
 
 
 
